d5e0392618610b65e3d7c9767046f2bad02f3370
David Josephs <davidjosephs12@gmail.com>
2020-10-07 00:31:39 -0400

    added functional programming directory * added 3 object iterator adaptor functions   * mapObj transforms input obj's values according to mapper callback.   * filterObj filters input obj's values according to predicate callback.   * reduceObj reduces input obj's values to single value according to reducer callback.

---
+++ src/fp/index.ts
@@ -0,0 +1,1 @@
+export { mapObj, filterObj, reduceObj } from "./obj";
---
+++ src/fp/obj.ts
@@ -0,0 +1,37 @@
+type Obj<T> = { [key: string]: T };
+type mappedObj<U> = { [key: string]: U };
+
+export function mapObj<T, U>(
+  mapper: (value: T, key?: string, obj?: Obj<T>) => U,
+  obj: Obj<T>
+): mappedObj<U> {
+  const keys = Object.keys(obj);
+  return keys.reduce((newObj, key) => {
+    newObj[key] = mapper(obj[key]);
+    return newObj;
+  }, {} as mappedObj<U>);
+}
+
+export function filterObj<T>(
+  predicate: (value: T, key?: string, obj?: Obj<T>) => boolean,
+  obj: Obj<T>
+): Obj<T> {
+  const keys = Object.keys(obj);
+  return keys.reduce((newObj, key) => {
+    if (predicate(obj[key])) newObj[key] = obj[key];
+    return newObj;
+  }, {} as Obj<T>);
+}
+
+export function reduceObj<T, R>(
+  reducer: (acc: T | R, value: T, key?: string, obj?: Obj<T>) => T | R,
+  obj: Obj<T>,
+  initialValue: T | R = Object.values(obj)[0]
+): T | R {
+  let result = initialValue;
+  const keys = Object.keys(obj);
+  for (const key of keys) {
+    result = reducer(result, obj[key]);
+  }
+  return result;
+}
